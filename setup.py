972aa5ac30fb35ee5cceb2e040f4764c66d19f6f
Ed Kenbers <git@kenbers.com>
2020-03-06 23:20:16 +0100

    Wheel build
    Working wheel build

--- setup.py
+++ setup.py
@@ -11,12 +11,19 @@ setup(
     description='A simple calculator',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/EdKenbers/kenbers-calculator',
+    url='https://github.com/EdKenbers/kcalculator',
     package_dir={'': 'src'},
-    python_requires='>=3.8',
+    packages=find_packages(where='src'),
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         'console_scripts': [
-            'kcalculator=kcalculator.calculatorgui:main'
+            'kcalculator=kcalculator:main'
     ]
     },
 )
